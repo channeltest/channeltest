805b5efc35cee8f1ceaeb05975042dd5bf57ae42
channeltest <channeltest>
2020-03-20 18:14:38 -0600

    Added two modes

--- leftright.js
+++ leftright.js
@@ -5,8 +5,8 @@ class Player {
       this.audioIndex = 0;
       this.playing = false;
       this.mode = "alternating"
-      for (var a of this.audios){
-          a.addEventListener('ended', this.nextAudio.bind(this));
+      for(var i=0; i<this.audios.length; i++){
+          this.audios[i].addEventListener('ended', this.nextAudio.bind(this));
       }
     }
 
@@ -18,30 +18,42 @@ class Player {
         return this.audios[this.audioIndex];
     }
 
-    nextAudio() {
+    nextAudio(event) {
         if (this.playing){
-            this.incrementCounter()
-            this.currentAudio().play()
+            if (this.mode == "Simultaneous"){
+                event.srcElement.play()
+            } else if (this.mode == "Alternating"){
+                this.incrementCounter()
+                this.currentAudio().play()
+            }
         }
     }
 
     play() {
         if (!this.playing){
             this.playing = true
-            if (this.mode == "simultaneous"){
-
-            } else if (this.mode == "alternating"){
+            if (this.mode == "Simultaneous"){
+                for(var i=0; i<this.audios.length; i++) this.audios[i].play();
+            } else if (this.mode == "Alternating"){
                 this.currentAudio().play();
             }
         }
     }
 
     stop() {
-        this.playing = false;
-        var current = this.currentAudio()
-        current.pause();
-        current.currentTime = 0;
-        this.audioIndex = 0;
+        
+        if (this.playing){
+            this.playing = false;
+            for(var i=0; i<this.audios.length; i++){
+                this.audios[i].pause();
+                this.audios[i].currentTime = 0;
+            }
+            this.audioIndex = 0;
+            this.playing = false;
+        } else {
+            
+        }
+        
     }
 
     incrementCounter() {
@@ -51,33 +63,26 @@ class Player {
 
 
 
-
-
 var left = new Audio('left.mp3');
 var right = new Audio('right.mp3')
 var myPlayer = new Player([left, right]);
-  
 
 
 
-function playAlternating() {
-    var button = document.getElementById("alternatingButton");
+function playClicked(type) {
+    var button = document.getElementById(type + "Button");
     button.classList.toggle('buttonPlaying');
     if (myPlayer.playing){
         myPlayer.stop()
+        if (myPlayer.mode != type){
+            var otherButton = document.getElementById(myPlayer.mode + "Button");
+            otherButton.classList.toggle('buttonPlaying');
+            myPlayer.mode = type
+            myPlayer.play()
+        }
     } else {
-       myPlayer.play();
-    }
-
-}
-
-function playSimultaneous() {
-    var button = document.getElementById("simultaneousButton");
-    button.classList.toggle('buttonPlaying');
-    if (myPlayer.playing){
-        myPlayer.stop()
-    } else {
-       myPlayer.play();
+        myPlayer.mode = type
+        myPlayer.play();
     }
 
 }
